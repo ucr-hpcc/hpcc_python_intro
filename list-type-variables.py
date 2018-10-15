cc292341ee209ee79877c96a6662c290fbaacbc9
Chuck Forsyth <forsythc@ucr.edu>
2018-10-15 16:58:51 -0700

    adding files

---
+++ list-type-variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+listone = [ 'abcd', 786 , 2.23, 'john', 70.2 ] 
+
+listtwo = [123, 'john'] 
+
+print(listone)            # Prints complete list 
+print(listone[0])         # Prints first element of the list 
+print(listone[1:3])       # Prints elements starting from 2nd to 4th 
+print(listone[2:])        # Prints elements starting from 3rd element 
+print(listtwo * 2)        # Prints list two times 
+print(listone + listtwo)  # Prints concatenated lists
---
+++ number-type-variables.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+x = 1           # integer
+y = 1.5         # float
+
+x = 7           # integer
+y = 2           # integer
+z = x/y         # division
+print(z)        # print the value of y
+#3               # result , integer
+
+x = 7.0         # float
+y = 2.0         # float 
+z = x/y         # division
+print(z)        # print the value of y
+#3.5             # result , float
+
